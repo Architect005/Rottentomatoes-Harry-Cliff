1112ed810e83b713f8e6dcf5c4da546898ed3197
Cliff OG <cog@localhost.localdomain>
2023-04-28 15:15:30 +0100

    [ADD]: about us

--- src/functions/api.request.ts
+++ src/functions/api.request.ts
@@ -1,4 +1,10 @@
 import fetcher from "./fetcher";
+import jwt from "jsonwebtoken";
+
+export const validateToken = (token: string) => {
+  const user = jwt.verify(token, "hello");
+  return user;
+};
 
 export function register(data: {
   name: string;
@@ -11,4 +17,12 @@ export function register(data: {
 
 export function login(data: {email: string; password: string}) {
   return fetcher("/login", data);
+}
+
+export function editUser(data: {
+  id: string;
+  email: string;
+  password: string;
+}) {
+  return fetcher("/edit-user", data);
 }
---
+++ src/pages/api/edit-user.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/functions/prisma";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, email } = req.body;
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: id as unknown as string,
+      },
+      data: {
+        name: name as unknown as string,
+        email: email as unknown as string,
+      },
+    });
+    console.log(id);
+  } catch (e) {
+    res.status(401);
+    res.json({ error: "Something went wrong." });
+    return;
+  }
+  return res.json({ status: 201, message: "User is updated succesfully." });
+};
+
+export default handler;
